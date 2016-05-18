67054705f20ee0b9ca030db77e821cd5718b7e16
KovaxG <georgesmth202@gmail.com>
2016-05-18 15:55:44 +0300

    Tried to expand code

--- AQTR-InternetOfThings/AQTR-InternetOfThings.ino
+++ AQTR-InternetOfThings/AQTR-InternetOfThings.ino
@@ -1,55 +1,70 @@
-// Pin data
-int SeIn_1 = 9;
-int SeOu_1 = 8;
-int SeIn_2 = 10;
-int SeOu_2 = 11;
+//////////////////////////////////////////////////////////////
+//| Name: Internet of Things: Traffic Counter              |//
+//|________________________________________________________|//
+//////////////////////////////////////////////////////////////
 
+#define SampleTime 200
+#define sensorNr 5
+#define inputPinsLength 10
+
+// Pin data
+int inputPins[] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
 void setup() {
-  pinMode (SeIn_1, INPUT_PULLUP);
-  pinMode (SeOu_1, INPUT_PULLUP);
-  pinMode (SeIn_2, INPUT_PULLUP);
-  pinMode (SeOu_2, INPUT_PULLUP);
+	for (int i = 0; i < inputPinsLength; i++) {
+		pinMode(inputPins[i], INPUT);
+	}
   
-  Serial.begin(9600);
-  while (!Serial) {}
+	Serial.begin(9600);
+	while (!Serial) {}
 }
 
-
-int t1 = 0;
+// Algoritm variables
+int t[] = {0, 0, 0, 0, 0};
 int in = 0;
 
-boolean XSI_1;
-boolean XSO_1;
-boolean SI_1;
-boolean SO_1;
+boolean SO[] = {false, false, false, false, false};
+boolean SI[] = {false, false, false, false, false};
+boolean XSO[] = {false, false, false, false, false};
+boolean XSI[] = {false, false, false, false, false};
 
 
-void loop() {
-  
-  SI_1 = !digitalRead(SeIn_1);
-  SO_1 = !digitalRead(SeOu_1);
-  
-  if (!SO_1 && !SI_1 && XSO_1) {
-    if (t1 == -1) in--;
-    t1++;
-    if (t1 > 1) t1 = 1;
-  }
-  if (!SI_1 && !SO_1 && XSI_1) {
-    if (t1 == 1) in++;
-    t1--;
-    if (t1 < -1) t1 = -1;
-  }
-  
-  Serial.print(" t1 = ");
-  Serial.print(t1);
-  Serial.print(" In = ");
-  Serial.println(in);
-  
-  XSI_1 = SI_1;
-  XSO_1 = SO_1;
-  
-  delay(200);
+void readAllSensors() {
+	for (int i = 0; i < sensorNr; i++) {
+		XSO[i] = SO[i];
+		XSI[i] = SI[i];
+	}
+	for (int i = 0; i < sensorNr; i += 2) {
+		SO[i] = !digitalRead(inputPins[i]);
+		SI[i] = !digitalRead(inputPins[i+1]);
+	}
 }
 
+void updateStates(int index) {
+	if (!SO[index] && !SI[index] && XSO[index]) {
+		if (t[index] == -1) in--;
+		t[index]++;
+		if (t[index] > 1) t[index] = 1;
+	}
+	if (!SI[index] && !SO[index] && XSI[index]) {
+		if (t[0] == 1) in++;
+		t[0]--;
+		if (t[0] < -1) t[0] = -1;
+	}
+}
 
+void loop() {
+
+	readAllSensors();
+	
+	for (int i = 0; i < sensorNr; i++) {
+		updateStates(i);
+	}
+  
+	Serial.print(" t1 = ");
+	Serial.print(t[0]);
+	Serial.print(" In = ");
+	Serial.println(in);
+
+	delay(SampleTime);
+}
